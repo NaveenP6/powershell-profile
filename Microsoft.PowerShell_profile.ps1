475074c28d761943f78dfeed6b8948b047e0c580
nphn600 <nphn600@gmail.com>
2023-07-30 09:10:13 -0400

    Edited Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -227,7 +227,7 @@ function pgrep($name) {
 ## Final Line to set prompt
 #oh-my-posh init pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/craver.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json" | Invoke-Expression
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
