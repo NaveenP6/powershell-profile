c2a285991a04396b3136d06bf2f72ee1d3140970
nphn600 <nphn600@gmail.com>
2023-11-26 20:35:39 -0500

    Edited Set-Location Function to "D;\Code"

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -137,7 +137,7 @@ Set-Alias -Name vim -Value $EDITOR
 
 
 function ll { Get-ChildItem -Path $pwd -File }
-function g { Set-Location D:\ }
+function g { Set-Location D:\Code }
 function gcom {
     git add .
     git commit -m "$args"
